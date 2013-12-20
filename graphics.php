7cb65b8840cd5a4a41b4daeb819f0714a771c117
epson121 <traxdater12@gmail.com>
2013-12-20 13:51:44 +0100

    Added php support (header loading)

---
+++ graphics.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lrrrr</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/modernizr.js"></script>
+  </head>
+  <body>
+
+    <?php include("header.html") ?>
+
+    <div class="row extra-margin-top-3-times">
+      <div class="large-3 large-offset-3 columns">
+        <a href="rg/applets.html" class="button large button-fixed-width">Java Applets</a>
+      </div>
+      <div class="large-6 columns">
+        <a href="rg/open_gl.html" class="button large button-fixed-width">Open GL</a>
+      </div>
+    </div>
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
