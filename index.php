96abc54751a9df67e1d35134e49e307e5a3c7eee
epson121 <traxdater12@gmail.com>
2014-01-10 20:56:54 +0100

    Added parallax scroll on index page.   Added portfolio-like index page

--- index.php
+++ index.php
@@ -1,20 +1,123 @@
-<!doctype html>
+  <!doctype html>
 <html class="no-js" lang="en">
   <?php include("header.html") ?>
+  
   <body>
     
     <?php include("nav-bar.html") ?>
-    <div class="row extra-margin-top" align="center">
-      <div class="under-construction centered">
-       Under construction</br></br>
-       <img src="img/work.png" alt="work" />
-      </div>
-    </div>
-    
-    <script src="js/jquery.js"></script>
+   
+    <div id="intro">
+    <div class="story">
+        <div class="float-left first">
+           <h2>Hello. My name is Luka.</h2>
+           <p class="black-p">
+              I consider myself a programming enthusiast, and an avid learner.</br>
+              I usually do Java/C#/C for school, but use other technologies for side-projects,
+              so I decided to create a small portfolio of my work (ones that can be shown via web).  </br>
+              As for my other stuff check out my <a href="http://github.com/epson121">github</a> profile, and my <a href="http://hr.linkedin.com/in/lurajcevi/">linkedIn page.</a></br>
+              <em>Scroll for more.</em>
+          </p>
+        </div>
+      </div> <!--.story-->
+  </div> <!--#intro-->
+  
+  <div id="second">
+    <div class="story"><div class="bg"></div>
+        <div class="float-left">
+              <h2>I do web stuff</h2>
+              <p class="black-p">
+                Web is everywhere. So in order to keep yourself "up-to-date" as a programmer, you must know it. Period.</br>
+                Being more of a <em>backend developer</em> type (as you've already recognized by my "awesome" webpage), I usually do Ruby (more often) or PHP (less often), 
+                but it sure is fun to try something new and edgy (<a href="http://emberjs.com/">javascript frameworks</a>, 
+                <a href="http://foundation.zurb.com/">css frameworks</a> or <a href="http://en.wikipedia.org/wiki/Parallax_scrolling">scroll parallax</a> 
+                like I just did) from time to time.
+              </p>
+              <ul class="side-nav">
+                <li><a href="http://localhost/Lrrrr/apps/ember-markdown/index.html">Markdown editor </a></li>
+                <li><a href="http://citidist.lrrrr.info">City distance. Enter a city. Any city. Or two.</a></li>
+                <li><a href="http://uconv.lrrrr.info">Convert your units for free!!</a></li>
+                <li><a href="http://alarm.lrrrr.info">Alarm Clock. It also shows you the wrong time. (I am so lazy...)</a></li>
+                <li><a href="http://bdconv.lrrrr.info">Binary-Decimal converter. For when you need one.. (like, never!)</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+      
+  </div> <!--#second-->
+  
+  <div id="third">
+    <div class="story">
+        <div class="float-left">
+            <h2>I am no stranger to android development</h2>
+              <p class="black-p">
+                I've said it earlier, "web is everywhere". That means web is also on your phone or tablet or even your <a href="http://en.wikipedia.org/wiki/Phablet">phablet</a>, 
+                therefore you have to know some of it.</br>
+                I did some a while ago, and I liked it. First as a school project, and then in my spare time. </br>
+                I too, like every other developer, have a pool of ideas for "The next killer app" but, emm, I have no time to pursue them.. Ughh, yeah. that's the reason..
+              </p>
+              <ul class="side-nav">
+                <li><a href="https://play.google.com/store/apps/details?id=kiwi.main&hl=en">Kiwi app - keep your credentials safe. School project.</a></li>
+                <li><a href="https://github.com/epson121/forumhr_mobile">Mobile version of forum.hr - I like this one</a></li>
+                <li><a href="https://github.com/epson121/howManyDrinks">Beer pitcher counter</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+  <div id="fourth">
+    <div class="story">
+        <div class="float-left">
+            <h2>Games and graphics</h2>
+              <p class="black-p">
+                I am not a gamer, but I've made a few games. Also I took a computer graphics class, and it was really fun. 
+                Check out the <a href="graphics.php">gallery</a>.
+              </p>
+              <ul class="side-nav">
+                <li><a href="http://www.codeskulptor.org/#user14_wKWCSAy2CS_54.py">BlackJack</a></li>
+                <li><a href="http://www.codeskulptor.org/#user13_m18WJ61w9LTWw7M_7.py">Pong (oh lord, not again..)</a></li>
+                <li><a href="http://www.codeskulptor.org/#user16_u5ckQMCPoA_10.py">Asteroids clone</a></li>
+                <li><a href="https://github.com/epson121/pygame.examples">Pygame examples</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+   <div id="fifth">
+    <div class="story">
+        <div class="float-left">
+            <h2 style="margin-top:-2.8em;">I experiment with new technologies, and "old" ones, too</h2>
+              <p class="black-p">
+                So many hipster technologies, so little time. </br>
+                But you can always spare some to do a little <a href="http://emberjs.com/">ember</a> or try out
+                <a href="http://getbootstrap.com/"> bootstrap</a>. </br>
+                Learning is cool. You can learn a lot on <a href="https://www.coursera.org/">Coursera</a>. I took about half a dozen courses and it was great.
+                From programming in SML and Racket in <a href="https://www.coursera.org/course/proglang">Programming Languages</a> to learning about 
+                <a href="https://www.coursera.org/course/dino101">dinosaurs</a> or <a href="https://www.coursera.org/course/bigbang">dark energy</a>, 
+                you can learn a whole lot about almost everything.
+              </p>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+   <div id="footer" style="text-align:center;color:grey;">
+      ©Luka Rajčević, 2014.
+   </div>
+   <script src="js/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.localscroll-1.2.7-min.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo-1.4.2-min.js"></script>
     <script>
       $(document).foundation();
+    </script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $('#nav').localScroll();
+        $('#intro').parallax("30%", 0.4);
+        $('#second').parallax("50%", 0.3);
+        $('#third').parallax("30%", 0.3);
+        $('#fourth').parallax("50%", 0.3);
+        $('#fifth').parallax("50%", 0.4);
+    })
     </script>
   </body>
 </html>
