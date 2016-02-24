822eed3f5f3939a2f65e2e9512992e75fe937c10
epson121 <traxdater12@gmail.com>
2016-02-24 20:12:18 +0000

    Changes

---
+++ applets.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <?php include("header.html") ?>
+  <body>
+
+    <?php include("nav-bar.html") ?>
+    <div class="row">
+      <div class="large-8 large-offset-2 columns" >
+        <ul id="applet_images" class="clearing-thumbs" data-clearing>
+        <li><a href="rg/img/applets/fan.png"><img src="rg/img/applets/thumb-fan.png"></a></li>
+        <li><a href="rg/img/applets/foi_logo.png"><img src="rg/img/applets/thumb-foi_logo.png"></a></li>
+        <li><a href="rg/img/applets/shape.png"><img src="rg/img/applets/thumb-shape.png"></a></li>
+        <li><a href="rg/img/applets/cone.png"><img src="rg/img/applets/thumb-cone.png"></a></li>
+        <li><a href="rg/img/applets/net.png"><img src="rg/img/applets/thumb-net.png"></a></li>
+        <li><a href="rg/img/applets/anemometer_1.png"><img src="rg/img/applets/thumb-anemometer_1.png"></a></li>
+        <li><a href="rg/img/applets/mirror.png"><img src="rg/img/applets/thumb-mirror.png"></a></li>
+        <li><a href="rg/img/applets/cyllinder.png"><img src="rg/img/applets/thumb-cyllinder.png"></a></li>
+        <li><a href="rg/img/applets/tractor.png"><img src="rg/img/applets/thumb-tractor.png"></a></li>
+        <li><a href="rg/img/applets/cube_rotation.png"><img src="rg/img/applets/thumb-cube_rotation.png"></a></li>
+        <li><a href="rg/img/applets/letter_f_perspective.png"><img src="rg/img/applets/thumb-letter_f_perspective.png"></a></li>
+        <li><a href="rg/img/applets/cardioid.png"><img src="rg/img/applets/thumb-cardioid.png"></a></li>
+        <li><a href="rg/img/applets/half_sphere.png"><img src="rg/img/applets/thumb-half_sphere.png"></a></li>
+        <li><a href="rg/img/applets/plane_3d.png"><img src="rg/img/applets/thumb-plane_3d.png"></a></li>
+        <li><a href="rg/img/applets/plane_2d.png"><img src="rg/img/applets/thumb-plane_2d.png"></a></li>
+        <li><a href="rg/img/applets/elipse_1.png"><img src="rg/img/applets/thumb-elipse_1.png"></a></li>
+        <li><a href="rg/img/applets/sine_wave.png"><img src="rg/img/applets/thumb-sine_wave.png"></a></li>
+        <li><a href="rg/img/applets/rotation_over_line.png"><img src="rg/img/applets/thumb-rotation_over_line.png"></a></li>
+        <li><a href="rg/img/applets/windmill.png"><img src="rg/img/applets/thumb-windmill.png"></a></li>
+        <li><a href="rg/img/applets/parabola.png"><img src="rg/img/applets/thumb-parabola.png"></a></li>
+        <li><a href="rg/img/applets/letter_f.png"><img src="rg/img/applets/thumb-letter_f.png"></a></li>
+        </ul>
+        <a href="graphics.php" style="color:black;">Back</a>
+      </div>
+    </div>
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/foundation/foundation.clearing.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,46 +1,142 @@
-<!doctype html>
+  <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lrrrr</title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/modernizr.js"></script>
-  </head>
+  <?php include("header.html") ?>
+  
   <body>
-
-    <?php include("header.html") ?>
-
-    <!-- ORBIT with images -->
-    <div class="row extra-margin-top" align="center">
-      <div class="small-6 small-centered columns">
-       <ul class="example-orbit" data-orbit>
-        <li>
-          <img src="assets/lrrr1.jpg"  alt="slide 1" />
-          <div class="orbit-caption">
-            Lrrrr..
-          </div>
-        </li>
-        <li>
-          <img src="assets/lrrr1.jpg" alt="slide 2" />
-          <div class="orbit-caption">
-            will..
-          </div>
-        </li>
-        <li>
-          <img src="assets/lrrr1.jpg" alt="slide 3" />
-          <div class="orbit-caption">
-            digest you!
-          </div>
-        </li>
-      </ul>
-      </div>
-    </div>
     
-    <script src="js/jquery.js"></script>
+    <?php include("nav-bar.html") ?>
+   
+    <div id="intro">
+    <div class="story">
+        <div class="float-left first">
+           <h2>Hello. My name is Luka.</h2>
+           <p class="black-p">
+              I consider myself a programming enthusiast, and an avid learner.</br>
+              My usual stack is Java/C#/C (for school purposes), but I use other technologies for side-projects,
+              so I decided to create a small portfolio of my work (ones that can be shown via web).  </br>
+              As for my other stuff check out my <a href="http://github.com/epson121">github</a> profile, and my <a href="http://hr.linkedin.com/in/lurajcevi/">linkedIn page.</a></br>
+              <em>Scroll for more.</em>
+          </p>
+        </div>
+      </div> <!--.story-->
+  </div> <!--#intro-->
+  
+  <div id="second">
+    <div class="story"><div class="bg"></div>
+        <div class="float-left">
+              <h2>I do web stuff</h2>
+              <p class="black-p">
+                Web is everywhere. So in order to keep yourself "up-to-date" as a programmer, you must know it.</br>
+                Being more of a <em>backend developer</em> type (as you've already recognized by my "awesome" webpage), I usually do Ruby (more often) or PHP (less often), 
+                but it sure is fun to try something new and edgy (<a href="http://emberjs.com/">javascript frameworks</a>, 
+                <a href="http://foundation.zurb.com/">css frameworks</a> or <a href="http://en.wikipedia.org/wiki/Parallax_scrolling">scroll parallax</a> 
+                like I just did) from time to time.
+              </p>
+              <ul class="side-nav">
+                <li><a href="http://localhost/Lrrrr/apps/ember-markdown/index.html">Markdown editor </a></li>
+                <li><a href="http://citidist.lrrrr.info">City distance. Enter a city. Any city. Or two.</a></li>
+                <li><a href="http://uconv.lrrrr.info">Convert your units for free!!</a></li>
+                <li><a href="http://alarm.lrrrr.info">Alarm Clock. It also shows you the wrong time. (I am so lazy...)</a></li>
+                <li><a href="http://bdconv.lrrrr.info">Binary-Decimal converter. For when you need one.. (like, never!)</a></li>
+                <li><a href="apps/circular_table/index.html">Circular table generator</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+      
+  </div> <!--#second-->
+  
+  <div id="third">
+    <div class="story">
+        <div class="float-left">
+            <h2>I am no stranger to android development</h2>
+              <p class="black-p">
+                First as a school project, and then in my spare time. </br>
+                Like every other developer, I also have a pool of ideas for "The next killer app" but, emm, I have no time to pursue them.. Ughh, yeah. that's the reason..
+              </br>
+              My usual platform is Android but it would be fun to try others as well.
+              </p>
+              <ul class="side-nav">
+                <li><a href="https://play.google.com/store/apps/details?id=kiwi.main&hl=en">Kiwi app - keep your credentials safe. School project.</a></li>
+                <li><a href="https://github.com/epson121/forumhr_mobile">Mobile version of forum.hr - I like this one</a></li>
+                <li><a href="https://github.com/epson121/howManyDrinks">Beer pitcher counter - fork from @presjednik</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+  <div id="fourth">
+    <div class="story">
+        <div class="float-left">
+            <h2>Games and graphics</h2>
+              <p class="black-p">
+                I am not a gamer, but I've made a few games. Also I took a computer graphics class, and it was really fun. 
+                Check out the <a href="graphics.php">gallery</a>.
+              </p>
+              <ul class="side-nav">
+                <li><a href="http://www.codeskulptor.org/#user14_wKWCSAy2CS_54.py">BlackJack</a></li>
+                <li><a href="http://www.codeskulptor.org/#user13_m18WJ61w9LTWw7M_7.py">Pong (oh lord, not again..)</a></li>
+                <li><a href="http://www.codeskulptor.org/#user16_u5ckQMCPoA_10.py">Asteroids clone</a></li>
+                <li><a href="https://github.com/epson121/pygame.examples">Pygame examples</a></li>
+              </ul>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+   <div id="fifth">
+    <div class="story">
+        <div class="float-left">
+            <h2 style="margin-top:-2.8em;">I experiment with new technologies, and "old" ones, too</h2>
+              <p class="black-p">
+                So many hipster technologies, so little time. </br>
+                But you can always spare some to do a little <a href="http://emberjs.com/">ember</a> or try out
+                <a href="http://getbootstrap.com/"> bootstrap</a>. </br>
+                Learning is cool. You can learn a lot on <a href="https://www.coursera.org/">Coursera</a>. I took about half a dozen courses and it was great.
+                From programming in SML and Racket in <a href="https://www.coursera.org/course/proglang">Programming Languages</a> to learning about 
+                <a href="https://www.coursera.org/course/dino101">dinosaurs</a> or <a href="https://www.coursera.org/course/bigbang">dark energy</a>, 
+                you can pick up a lot of knowledge about almost anything.
+              </p>
+          </div>
+      </div> <!--.story-->
+  </div> <!--#third-->
+
+   <div id="footer" style="text-align:center;color:grey;">
+      ©Luka Rajčević, 2014.
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- lrrr_index -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:320px;height:50px"
+     data-ad-client="ca-pub-4244695740085640"
+     data-ad-slot="1779249411"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Ad -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-4244695740085640"
+     data-ad-slot="9443300210"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+   </div>
+   <script src="js/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.localscroll-1.2.7-min.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo-1.4.2-min.js"></script>
     <script>
       $(document).foundation();
     </script>
-  </body>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        //$('#nav').localScroll();
+        //$('#intro').parallax("30%", 0.4);
+        //$('#second').parallax("50%", 0.3);
+        //$('#third').parallax("30%", 0.3);
+        //$('#fourth').parallax("50%", 0.3);
+        //$('#fifth').parallax("50%", 0.4);
+    })
+    </script>
+</body>
 </html>
